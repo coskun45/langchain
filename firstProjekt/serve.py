85e5b125a824b22f7fe4f45dbe9074eccb27c017
Eyüp Coskun <eyuep.coskun@adesso.de>
2024-12-09 14:33:29 +0100

    langserve playground with chain structure

--- firstProjekt/serve.py
+++ firstProjekt/serve.py
@@ -1,36 +1,46 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain.output_parsers import Json
+#!/usr/bin/env python
+from typing import List
+
 from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
 from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
 
-llm = ChatOpenAI(
-    model="gpt-3.5-turbo",
-    temperature=0,)
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant that translates {input_language} to {output_language}.",
-        ),
-        ("human", "{input}"),
-    ]
+# 2. Create model
+model = ChatOpenAI()
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
 )
 
-chain = prompt | llm
-app =FastAPI()
+# 5. Adding chain route
+
 add_routes(
     app,
     chain,
-    path="/chain"
+    path="/chain",
 )
 
 if __name__ == "__main__":
-    print(chain.invoke(
-    {
-        "input_language": "English",
-        "output_language": "German",
-        "input": "I love programming.",
-    }
-))
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8002)
